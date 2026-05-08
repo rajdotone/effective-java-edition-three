d30d48a949c3ab9de27f4f51d56aabf59da53b36
raj <rajmaharjan.one@gmail.com>
2026-05-08 16:12:34 -0700

    add enum

--- effective-java-edition-three/src/main/java/chapter2/item1/Point.java
+++ effective-java-edition-three/src/main/java/chapter2/item1/Point.java
@@ -4,6 +4,12 @@ public class Point {
 
     double x, y;
 
+    enum Angle {
+        RAD,
+        DEG
+    }
+    Angle angle;
+
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
@@ -18,6 +24,24 @@ public class Point {
         this.x = rho * Math.cos(phi);
         this.y = rho * Math.sin(phi);
     }
+    /**
+     *    * @param rho
+     *    * @param phi
+     *    * @param unit : enum is either "deg" or "rad"
+     */
+    public Point(double rho, double phi, Angle unit) {
+        switch (unit) {
+            case RAD:
+                this.x = rho * Math.cos(phi);
+                this.y = rho * Math.sin(phi);
+                break;
+            case DEG:
+                double phi1 = phi * Math.toRadians(phi);
+                this.x = rho * Math.cos(phi);
+                this.y = rho * Math.sin(phi);
+                break;
+        }
+    }
 
     @Override
     public String toString() {
@@ -31,7 +55,10 @@ public class Point {
         var p1 = new Point(1, 2);
         System.out.println(p1);
 
-        var p2 = new Point(3, Math.toRadians(4));
+        var p2 = new Point(5.0, Math.atan2(4.0, 3.0), -1);
         System.out.println(p2);
+
+        var p3 = new Point(5.0, Math.atan2(4.0, 3.0), Angle.RAD);
+        System.out.println(p3);
     }
 }
